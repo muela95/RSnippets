584f1ad05a90a319d220e29a7840f6d0b8a41836
muela95 <pmuela95@gmail.com>
2022-10-10 09:38:39 +0200

    Update Correlaciones mayores.R

--- Correlaciones mayores.R
+++ Correlaciones mayores.R
@@ -1,3 +1,6 @@
 correl <- df %>% correlate()
-correl[abs(correl$coef_corr) >= 0.6,] %>% print(n = Inf)
+signif <- correl[abs(correl$coef_corr) >= 0.6,] %>% print(n = Inf)
 # te saca todas y replicadas, habría que meter un filtro 
+# como chapuza puedes hacer:
+# signifFiltrada <- signif[!duplicated(signif$coef_corr),]
+# Que quita los valores de correlaciones que sean iguales
