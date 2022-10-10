c709a440575197339dddb02523ecb01f3f9751ff
muela95 <pmuela95@gmail.com>
2022-10-10 12:09:14 +0200

    Update Multiples ANOVAS.R

--- Multiples ANOVAS.R
+++ Multiples ANOVAS.R
@@ -1,12 +1,15 @@
-for(i in 29:ncol(df))
+lista4 <- c()
+for(i in 29:ncol(nvl4))
 {
     
-    column <- names(nvl3[i])
-    avz <- aov(get(column) ~ Grupo, data = df)
+    column <- names(nvl4[i])
+    avz <- aov(get(column) ~ Grupo, data = nvl4)
     if(summary(avz)[[1]][["Pr(>F)"]][1] < 0.05) {
-        
         print(column)
         print(summary(avz))
         print(TukeyHSD(avz))
+        lista4 <- append(lista4, column)
+        print(lista4)
+        
     }
 }
